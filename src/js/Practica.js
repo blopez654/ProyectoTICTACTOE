e452a51ad116165045086d35dc6e1ab4a0476077
blopez654 <blopez@fwdcostarica.com>
2024-08-09 12:19:04 -0600

    cambios

--- src/js/Practica.js
+++ src/js/Practica.js
@@ -1,15 +1,19 @@
 const cont = document.getElementsByClassName("cont");
 const etiquetaP = document.getElementById("etiquetaP")
+const boton = document.getElementById("boton")
+
+let registro = (localStorage.getItem("registro"))||[]; 
+
 
 for (let index = 0; index < cont.length; index++) {
 
     cont [index].addEventListener("click", function () {
 
        if (cont[index].textContent== "") {
-         cont[index].textContent= "X";
+         cont[index].textContent= "❤️";
          
        
-       let listaVacia = [];
+       let listaVacia = [];   
    
        for (let index = 0; index < cont.length; index++) {
          if (cont[index].textContent === "") {
@@ -22,87 +26,87 @@ for (let index = 0; index < cont.length; index++) {
          
         if ( listaVacia.length > 0 ) {  
           let numeroA = Math.floor(Math.random()*listaVacia.length)
-          listaVacia[numeroA].textContent="O";
+          listaVacia[numeroA].textContent="🖤";
           
         }
          
       }
 
  
-
-   if (cont[0].textContent==="X" && cont[1].textContent==="X" && cont[2].textContent==="X")  {
+   if (cont[0].textContent==="❤️" && cont[1].textContent==="X" && cont[2].textContent==="X")  {
       etiquetaP.textContent = "¡Ganaste!"
+     
    }
     
-   if (cont[3].textContent==="X" && cont[4].textContent==="X" && cont[5].textContent==="X") {
+   if (cont[3].textContent==="❤️" && cont[4].textContent==="X" && cont[5].textContent==="X") {
     etiquetaP.textContent = "¡Ganaste!"
+    
    }
  
-   if (cont[6].textContent==="X" && cont[7].textContent==="X" && cont[8].textContent==="X") {
+   if (cont[6].textContent==="❤️" && cont[7].textContent==="X" && cont[8].textContent==="X") {
     etiquetaP.textContent = "¡Ganaste!"
+    
    }
 
-   if (cont[0].textContent==="X" && cont[3].textContent==="X" && cont[6].textContent==="X") {
+   if (cont[0].textContent==="❤️" && cont[3].textContent==="X" && cont[6].textContent==="X") {
     etiquetaP.textContent = "¡Ganaste!"
+    
    }
 
-   if (cont[1].textContent==="X" && cont[4].textContent==="X" && cont[7].textContent==="X") {
+   if (cont[1].textContent==="❤️" && cont[4].textContent==="❤️" && cont[7].textContent==="❤️") {
    etiquetaP.textContent = "¡Ganaste!"
    }
  
-   if (cont[2].textContent==="X" && cont[5].textContent==="X" && cont[8].textContent==="X") {
+   if (cont[2].textContent==="❤️" && cont[5].textContent==="❤️" && cont[8].textContent==="❤️") {
    etiquetaP.textContent = "¡Ganaste!"
    }
 
-   if (cont[0].textContent==="X" && cont[4].textContent==="X" && cont[8].textContent==="X") {
+   if (cont[0].textContent==="❤️" && cont[4].textContent==="❤️" && cont[8].textContent==="❤️") {
     etiquetaP.textContent = "¡Ganaste!"
    }
+   
 
-   if (cont[2].textContent==="X" && cont[4].textContent==="X" && cont[6].textContent==="X") {
+   if (cont[2].textContent==="❤️" && cont[4].textContent==="❤️" && cont[6].textContent==="❤️") {
     etiquetaP.textContent = "¡Ganaste!"
    };
     
-   if (cont[0].textContent==="O" && cont[1].textContent==="O" && cont[2].textContent==="O") {
+   if (cont[0].textContent==="🖤" && cont[1].textContent==="🖤" && cont[2].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    };
 
-   if (cont[3].textContent==="O" && cont[4].textContent==="O" && cont[5].textContent==="O") {
+   if (cont[3].textContent==="🖤" && cont[4].textContent==="🖤" && cont[5].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    };
   
-   if (cont[6].textContent==="O" && cont[7].textContent==="O" && cont[8].textContent==="O") {
+   if (cont[6].textContent==="🖤" && cont[7].textContent==="🖤" && cont[8].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    };
 
-   if (cont[0].textContent==="O" && cont[3].textContent==="O" && cont[6].textContent==="O") {
+   if (cont[0].textContent==="🖤" && cont[3].textContent==="🖤" && cont[6].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    };
 
-   if (cont[1].textContent==="O" && cont[4].textContent==="O" && cont[7].textContent==="O") {
+   if (cont[1].textContent==="🖤" && cont[4].textContent==="🖤" && cont[7].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    }
  
-   if (cont[2].textContent==="O" && cont[5].textContent==="O" && cont[8].textContent==="O") {
+   if (cont[2].textContent==="🖤" && cont[5].textContent==="🖤" && cont[8].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    }
 
-   if (cont[0].textContent==="O" && cont[4].textContent==="O" && cont[8].textContent==="O") {
+   if (cont[0].textContent==="🖤" && cont[4].textContent==="🖤" && cont[8].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
    }
  
-   if (cont[2].textContent==="O" && cont[4].textContent==="O" && cont[6].textContent==="O") {
+   if (cont[2].textContent==="🖤" && cont[4].textContent==="🖤" && cont[6].textContent==="🖤") {
     etiquetaP.textContent = "¡Perdiste!"
     
    }
+   
+})
+ 
 
-
-
-
-
-
-
-   })
-};
+}
     
 
 
